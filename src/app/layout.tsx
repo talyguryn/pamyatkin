357110fa562690d1e6b9f40bd2bdf4170e66ec56
Taly <vitalik7tv@yandex.ru>
2025-05-25 20:19:02 +0300

    disable meta viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
+
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Памяткин',
   description: 'Сервис для создания памяток',
+};
+
+export const viewport: Viewport = {
+  initialScale: undefined,
+  width: undefined,
 };
 
 export default function RootLayout({
@@ -15,9 +21,6 @@ export default function RootLayout({
     <html lang="ru">
       <head>
         <link rel="icon" href="/icon.png" type="image/png" sizes="any" />
-
-        {/* Temporary disable viewport for mobiles */}
-        <meta name="viewport" content="viewport-fit=cover" />
       </head>
       <body className="antialiased">{children}</body>
     </html>
