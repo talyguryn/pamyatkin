3b6bdf9d7e72d46601f975575a4b7565215bb2b9
Taly <vitalik7tv@yandex.ru>
2025-05-25 19:39:48 +0300

    disable viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
     <html lang="ru">
       <head>
         <link rel="icon" href="/icon.png" type="image/png" sizes="any" />
+
+        {/* Temporary disable viewport for mobiles */}
+        <meta name="viewport" content="viewport-fit=cover" />
       </head>
       <body className="antialiased">{children}</body>
     </html>
